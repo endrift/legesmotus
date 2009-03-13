b16ac4ef6e81302359428578f923d27e43ed5d6d
Nathan Partlan <npartlan@techhouse.org>
2009-03-13 17:43:55 +0000

    Added basic user-input parsing skeleton. Can now press Q to quit the game. FPS is now locked.
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@37 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

--- client/GameController.cpp
+++ client/GameController.cpp
@@ -16,38 +16,92 @@
 using namespace std;
 
 GameController::GameController() {
-	m_screen_width = 1024;
-	m_screen_height = 768;
-	m_pixel_depth = 24;
-	m_fullscreen = true;
-	m_window = GameWindow::get_instance(m_screen_width, m_screen_height, m_pixel_depth, m_fullscreen);
+	init(1024, 768, 24, true);
 }
 
 GameController::GameController(int width, int height) {
-	m_screen_width = width;
-	m_screen_height = height;
-	m_pixel_depth = 24;
-	m_fullscreen = true;
-	m_window = GameWindow::get_instance(m_screen_width, m_screen_height, m_pixel_depth, m_fullscreen);
+	init(width, height, 24, true);
 }
 
 GameController::~GameController() {
 	m_window->destroy_instance();
 }
 
+void GameController::init(int width, int height, int depth, bool fullscreen) {
+	m_screen_width = width;
+	m_screen_height = height;
+	m_pixel_depth = depth;
+	m_fullscreen = fullscreen;
+	m_quit_game = false;
+	m_window = GameWindow::get_instance(m_screen_width, m_screen_height, m_pixel_depth, m_fullscreen);
+}
+
 void GameController::run(int lockfps) {
 	cerr << "SDL window is: " << m_window->get_width() << " pixels wide and " 
 		<< m_window->get_height() << " pixels tall." << endl;
 	
-	int i = 0;
-	while(i < 5) {
-		m_window->redraw();
-		SDL_Delay(1000);
-		i++;
+	unsigned long startframe = SDL_GetTicks();
+	
+	/* 1 second / FPS = milliseconds per frame */
+	unsigned long delay = 1000 / lockfps;
+	
+	while(m_quit_game == false) {
+		process_input();
+		
+		if (m_quit_game == true) {
+			break;
+		}
+		
+		/* Update graphics if frame rate is correct */
+		unsigned long currframe = SDL_GetTicks();
+		if((currframe - startframe) >= delay) {
+			// this corrects for fast/slow framerate:
+			// multiply all position changes by this variable to keep game speed constant.
+			float timesync = ((currframe - startframe) / delay);
+			
+			// the framerate:
+			int framerate = (1000/(currframe - startframe));
+			
+			m_window->redraw();
+			startframe = SDL_GetTicks();
+		}
 	}
 }
 
 void GameController::set_screen_dimensions(int width, int height) {
 	m_screen_width = width;
 	m_screen_height = height;
+}
+
+void GameController::process_input() {
+	SDL_Event event;
+	
+	while(SDL_PollEvent(&event) != 0) {
+		switch(event.type) {
+			case SDL_KEYDOWN:
+				//Check which key using: event.key.keysym.sym == SDLK_<SOMETHING>
+				break;
+
+			case SDL_KEYUP:
+				break;
+				
+			case SDL_MOUSEMOTION:
+				break;
+				
+			case SDL_MOUSEBUTTONDOWN:
+				break;
+				
+			case SDL_MOUSEBUTTONUP:
+				break;
+				
+			default:
+				break;
+		}
+	}
+	// For keys that can be held down:
+   	m_keys = SDL_GetKeyState(NULL);
+	if (m_keys[SDLK_q]) {
+		cerr << "Q pressed - quitting." << endl;
+		m_quit_game = true;
+	}
 }
--- client/GameController.hpp
+++ client/GameController.hpp
@@ -20,13 +20,17 @@ private:
 	int 		m_screen_height;
 	int 		m_pixel_depth;
 	bool 		m_fullscreen;
+	bool		m_quit_game;
+	Uint8*		m_keys;
 	
+	void		init(int width, int height, int depth, bool fullscreen);
+	void		process_input();
 public:
 	GameController();
 	GameController(int width, int height);
 	~GameController();
 	
-	void		run(int lockfps=-1);
+	void		run(int lockfps=60);
 	void		set_screen_dimensions(int width, int height);
 };
 
