fc708b580e3eb3e289401ff567fa1e6ae2c307e5
Jeffrey Pfau <jeffrey@endrift.com>
2009-03-25 06:28:10 +0000

    Fix common math files
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@74 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

---
+++ common/math.cpp
@@ -0,0 +1,11 @@
+/*
+ * common/math.cpp
+ *
+ * Copyright 2009 - Nathan Partlan, Andrew Ayer, Daniel Schneider, and Jeffrey Pfau
+ * 
+ */
+
+#include "math.hpp"
+
+const double RADIANS_TO_DEGREES = 57.29577951308232;
+const double DEGREES_TO_RADIANS = 1.745329251994e-02;
--- common/math.hpp
+++ common/math.hpp
@@ -5,6 +5,9 @@
  * 
  */
 
+#ifndef LM_COMMON_MATH_H
+#define LM_COMMON_MATH_H
+
 #include <stdint.h>
 #include <cmath>
 
@@ -12,8 +15,8 @@
 #define M_PI 3.141592653589793
 #endif
 
-const double RADIANS_TO_DEGREES = 57.29577951308232;
-const double DEGREES_TO_RADIANS = 1.745329251994e-02;
+extern const double RADIANS_TO_DEGREES;
+extern const double DEGREES_TO_RADIANS;
 
 static inline uint32_t toPow2(uint32_t num) {
 	--num;
@@ -23,3 +26,5 @@ static inline uint32_t toPow2(uint32_t num) {
 	++num;
 	return num;
 }
+
+#endif
