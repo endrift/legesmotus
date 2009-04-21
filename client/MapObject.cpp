bbb055191b1f81df58db4be555e06655f43e1535
Andrew Ayer <agwadude@users.sourceforge.net>
2009-04-21 20:33:17 +0000

    Sprites for map objects drawn in correct place, relative to their center.
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@138 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

--- client/MapObject.cpp
+++ client/MapObject.cpp
@@ -17,8 +17,9 @@ MapObject::MapObject(int type, Point upper_left) {
 void MapObject::set_sprite(Graphic* s) {
 	m_sprite = s;
 	if (m_sprite != NULL) {
-		m_sprite->set_x(m_upper_left.x);
-		m_sprite->set_y(m_upper_left.y);
+		// NB: sprites are drawn from center
+		m_sprite->set_x(m_upper_left.x - m_sprite->get_image_width() / 2);
+		m_sprite->set_y(m_upper_left.y - m_sprite->get_image_height() / 2);
 	}
 }
 
