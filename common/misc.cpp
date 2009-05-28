0a36ce201f787e20d17190cba39925bfdb7ddce2
Andrew Ayer <agwadude@users.sourceforge.net>
2009-05-28 04:12:47 +0000

    Changed copyright notice in every file, modified add_class_file to produce the new notice, and added a copy of the GPL to the root of the source code tree.
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@380 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

--- common/misc.cpp
+++ common/misc.cpp
@@ -1,7 +1,24 @@
 /*
  * common/misc.cpp
  *
- * Copyright 2009 - Nathan Partlan, Andrew Ayer, Daniel Schneider, and Jeffrey Pfau
+ * This file is part of Leges Motus, a networked, 2D shooter set in zero gravity.
+ * 
+ * Copyright 2009 Andrew Ayer, Nathan Partlan, Jeffrey Pfau
+ * 
+ * Leges Motus is free and open source software.  You may redistribute it and/or
+ * modify it under the terms of version 2, or (at your option) version 3, of the
+ * GNU General Public License (GPL), as published by the Free Software Foundation.
+ * 
+ * Leges Motus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the full text of the GNU General Public License for
+ * further detail.
+ * 
+ * For a full copy of the GNU General Public License, please see the COPYING file
+ * in the root of the source code tree.  You may also retrieve a copy from
+ * <http://www.gnu.org/licenses/gpl-2.0.txt>, or request a copy by writing to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
  * 
  */
 
