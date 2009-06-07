4b707f5d07e8550a143786da54ff2461719493d1
Jeffrey Pfau <jeffrey@endrift.com>
2009-06-07 02:15:23 +0000

    Add non-debug build options
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@435 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

--- client/GameController.cpp
+++ client/GameController.cpp
@@ -39,8 +39,9 @@
 #include "common/timer.hpp"
 
 #include <vector>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <limits>
 #include <sstream>
