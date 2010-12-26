c3e8f19a85f71655ff9548b6abf6c27f95a04576
Nathan Partlan <npartlan@techhouse.org>
2010-12-26 23:11:41 +0000

    Added Penetration gun type. Could probably use a bit more testing.
    git-svn-id: https://legesmotus.svn.sourceforge.net/svnroot/legesmotus/legesmotus/trunk@1058 a5a5d5d7-22d1-44d1-b37a-67d3d99fcfd7

--- common/Weapon.cpp
+++ common/Weapon.cpp
@@ -25,6 +25,7 @@
 #include "Weapon.hpp"
 #include "common/WeaponReader.hpp"
 #include "common/StandardGun.hpp"
+#include "common/PenetrationGun.hpp"
 #include <cstring>
 
 #include <iostream>
@@ -56,7 +57,7 @@ Weapon*	Weapon::new_weapon (WeaponReader& data) {
 	//} else if (strcasecmp(weapon_type, "charge") == 0) { 
 	} else if (strcasecmp(weapon_type, "penetration") == 0)	{
 		cerr << "Penetration." << endl;
-		return new StandardGun(weapon_id, data);
+		return new PenetrationGun(weapon_id, data);
 	} else {
 		cerr << "Error: unknown weapon type: " << weapon_type << endl;
 	}
